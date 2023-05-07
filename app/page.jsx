ff94e8ed71a8106cd05440737d77887005e27eb2
mcbashkov <126468509+mcbashkov@users.noreply.github.com>
2023-05-07 19:48:52 +0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import Feed from "@components/Feed";
 const Home = () => (
   <section className='w-full flex-center flex-col'>
     <h1 className='head_text text-center'>
-      The First AI-Powered Social Platform in 
+      The First AI Social Platform From 
       <br className='max-md:hidden' />
       <span className='orange_gradient text-center'>Indonesia</span>
     </h1>
