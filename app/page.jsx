48ed0230f667ac40f67cbe9e40755b352475b384
mcbashkov <126468509+mcbashkov@users.noreply.github.com>
2023-05-07 19:21:28 +0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -3,12 +3,12 @@ import Feed from "@components/Feed";
 const Home = () => (
   <section className='w-full flex-center flex-col'>
     <h1 className='head_text text-center'>
-      Mencari & Berbagi
+      The First AI-powered social platform 
       <br className='max-md:hidden' />
-      <span className='orange_gradient text-center'> Prompt AI</span>
+      <span className='orange_gradient text-center'> in Indonesia</span>
     </h1>
     <p className='desc text-center'>
-      ThinkTank adalah platform open-source untuk mencari dan berbagi "Prompt" AI
+      Dedicated to the exploration and exchange of groundbreaking "Prompt" Ideas and Innovations
     </p>
 
     <Feed />
